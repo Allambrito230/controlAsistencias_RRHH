b8b33db90d4f6bebaa2c73ea08e504c7562aabe4
Allan-Arnoldo-Torres-Flores <allantorres230@gmail.com>
2025-01-24 16:22:03 -0600

    auth funcional, pero feo

---
+++ gestionAsistenciaPermisos/auth/urls.py
@@ -0,0 +1,11 @@
+# gestionAsistenciaPermisos/auth/urls.py
+from django.urls import path
+from . import views
+from django.contrib.auth.forms import UserCreationForm
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('base/', views.base, name='base'),
+    path('signin/', views.signin, name='signin'),
+]
--- gestionAsistenciaPermisos/auth/views.py
+++ gestionAsistenciaPermisos/auth/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login as login_sesion, logout, authenticate
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'login.html')
+    # return HttpResponse("Hello World")
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'formulario': UserCreationForm()})
+        # print('Enviando formulario')
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # Registrar usuario
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login_sesion(request, user)
+                # return HttpResponse('Usuario registrado correctamente')
+                # return render(request, 'signup.html', {'formulario': UserCreationForm(), 'mensaje': 'Usuario registrado correctamente'})
+                return redirect('base')
+            except:
+                # return HttpResponse('El usuario ya existe')
+                return render(request, 'signup.html', {'formulario': UserCreationForm(), 'mensaje': 'El usuario ya existe'})
+        else:
+            return HttpResponse('Las contraseñas no coinciden')
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def signin(request):
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {'formulario': AuthenticationForm()})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {'formulario': AuthenticationForm(), 'mensaje': 'Usuario o contraseña incorrectos'})
+        else:
+            login_sesion(request, user)
+            return redirect('base')
+        # print(request.POST)
+        # return render(request, 'signin.html', {'formulario': AuthenticationForm()})
+    # return render(request, 'signin.html', {'formulario': AuthenticationForm()})
