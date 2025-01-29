c544eec659c0009413d9f048d3c5d16248eb355b
spineda <eunwoomivaron67@gmail.com>
2025-01-29 09:02:00 -0600

    Permisos Funcional

--- Apps/permisos/urls.py
+++ Apps/permisos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . views import exportar_permisos_excel
 
 
 urlpatterns = [
@@ -7,11 +8,12 @@ urlpatterns = [
     # path('verificar_solicitud_activa/', views.verificar_solicitud_activa_view, name='verificar_solicitud_activa'),
     # path('Permisos/solicitud/exito/', views.exito_view, name='permisos_solicitud_exito'),
 
-    # path('Permisos/permisos_gestion', views.permisos_gestion_view, name='permisos_gestion'),
+    path('Permisos/permisos_gestion', views.permisos_gestion_view, name='permisos_gestion'),
     path('permisos_historial/', views.permisos_historial_view, name='permisos_historial'),
     path('Listas/departamentosJson/', views.lista_departamentos_view, name='lista_departamentos'),
     path('Listas/empresasJson/', views.lista_empresas_view, name='lista_empresas'),
     path('Listas/sucursalesJson/', views.lista_sucursales_view, name='lista_sucursales'),
-     path('colaboradores/<int:departamento_id>/', views.colaboradores_por_departamento_view, name='colaboradores_por_departamento'),
-
+    path('colaboradores/<int:departamento_id>/', views.colaboradores_por_departamento_view, name='colaboradores_por_departamento'),
+    
+    path("exportar_permisos_excel/", exportar_permisos_excel, name="exportar_permisos_excel"),
 ]
